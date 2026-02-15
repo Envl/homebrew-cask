a75a70d77722b456473b317f6a5eb9dfbc7da25e
envl <yyaomingm@gmail.com>
2026-02-15 21:15:30 +0800

    prisma-launch 1.0.17

---
+++ Casks/prisma-launch.rb
@@ -0,0 +1,11 @@
+cask "prisma-launch" do
+  version "1.0.17"
+  sha256 "714e684aff693a22d22ff580f1217748dc8994c8d0df9fb62262076216ebd9f6"
+
+  url "https://storage.lumik.space/prisma/PrismaLaunch-1.0.17.dmg"
+  name "Prisma Launch"
+  desc "Launchpad-style app launcher overlay"
+  homepage "https://github.com/envl/tahoe-launchpad"
+
+  app "PrismaLaunch.app"
+end
